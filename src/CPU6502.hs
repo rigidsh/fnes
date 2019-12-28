7037bad8ce604cbf3c54034eb5389a7807ba23d4
Aleksandr Petrov <rigid.sh@gmail.com>
2019-12-28 15:53:51 +0200

    move memory to new module

--- src/CPU6502.hs
+++ src/CPU6502.hs
@@ -5,6 +5,7 @@ module CPU6502 where
 
 import System.Environment
 import Data.Bits
+import Memory
 
 
 data Registers6502 = Registers6502 { 
@@ -236,65 +237,3 @@ dey (_, cpu) = updateYRegister (1-) cpu
 -- S
 sta :: (MemoryAccessor, CPU6502) -> CPU6502
 sta (arg, cpu) = writeValue arg $ getAccRegister cpu
-
-
---- MEMORY ---
-class Memory a where
-    read8 :: Int -> a -> Int
-    read16 :: Int -> a -> (Int, Int)
-    write8 :: Int -> Int -> a -> a
-    write16 :: Int -> (Int, Int) -> a -> a
-
-type ListMemory = [[Int]] 
-
-
-decodeAddress :: Int -> (Int, Int)
-decodeAddress address = ( shiftR (address .&. 0xFF00) 8, address .&. 0xFF)
-
-instance Memory ListMemory where
-    read8 address memory = read8FromPage (getMemoryPage memory pageNumber) offset where 
-        (pageNumber, offset) = decodeAddress address
-    read16 address memory= read16FromPage (getMemoryPage memory pageNumber) offset where 
-        (pageNumber, offset) = decodeAddress address
-    write8 address value memory = updateMemoryPage memory pageNumber $ write8ToPage offset value where
-        (pageNumber, offset) = decodeAddress address
-    write16 address value memory = updateMemoryPage memory pageNumber $ write16ToPage offset value where
-        (pageNumber, offset) = decodeAddress address
-
-updateMemoryPage :: ListMemory -> Int -> ([Int] -> [Int]) -> ListMemory
-updateMemoryPage ( (currentPage:restPages)) pageNumber updater
-    | pageNumber == 0 =  ((updater currentPage):restPages)
-    | otherwise =  (currentPage:t) where
-         t = updateMemoryPage ( restPages) (pageNumber - 1) updater 
-updateMemoryPage [] pageNumber updater
-    | pageNumber == 0 =  [(updater emptyPage)]
-    | otherwise = (emptyPage:t) where
-         t = updateMemoryPage [] (pageNumber - 1) updater
-
-emptyPage :: [Int]
-emptyPage = take 256 (repeat 0)
-
-getMemoryPage :: ListMemory -> Int -> [Int]
-getMemoryPage [] _ = emptyPage 
-getMemoryPage ( (firstPage:otherPages)) pageNumber 
-    | pageNumber == 0 = firstPage
-    | otherwise = getMemoryPage ( otherPages) (pageNumber - 1)
-
-read8FromPage :: [Int] -> Int -> Int
-read8FromPage page offset = page!!offset
-
--- TODO: optimize
-read16FromPage :: [Int] -> Int -> (Int, Int)
-read16FromPage page address = (read8FromPage page (address + 1), read8FromPage page address)
-
-write8ToPage :: Int -> Int -> [Int] -> [Int]
-write8ToPage offset value (x:xs) 
-    | offset == 0 = value:xs
-    | otherwise = x:write8ToPage (offset - 1) value xs
-write8ToPage offset value []
-    | offset == 0 = [value]
-    | otherwise = 0:write8ToPage (offset - 1) value [] 
-
-write16ToPage :: Int -> (Int, Int) -> [Int] -> [Int]
-write16ToPage offset (b2, b1) page = write8ToPage (offset + 1) b2 $write8ToPage offset b1 page
-
